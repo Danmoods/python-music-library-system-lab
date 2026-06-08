601dcb1023785a254c51425f7056a4b1ed8576db
Danmoods <saitamang1234@gmail.com>
2026-06-08 08:40:32 +0300

    Created a python-music-library-system

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,36 @@
 class Song:
-    pass
+    # Class attributes
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        # Instance attributes
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        # Increment the count
+        Song.count += 1
+
+        # Add genre to genres list if not already present
+        if genre not in Song.genres:
+            Song.genres.append(genre)
+
+        # Add artist to artists list if not already present
+        if artist not in Song.artists:
+            Song.artists.append(artist)
+
+        # Update genre_count
+        if genre in Song.genre_count:
+            Song.genre_count[genre] += 1
+        else:
+            Song.genre_count[genre] = 1
+
+        # Update artist_count
+        if artist in Song.artist_count:
+            Song.artist_count[artist] += 1
+        else:
+            Song.artist_count[artist] = 1
